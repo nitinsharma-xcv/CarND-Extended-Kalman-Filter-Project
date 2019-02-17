7653f2acd590efc65744a34b33eb4dcedfbd7ca1
nitinsharma-xcv <nitinsharma.xcv@gmail.com>
2019-02-17 13:23:15 +0530

    rmse

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,9 +11,9 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
